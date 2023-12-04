bd2385ca08679f868cff904bc96995cc66745b4d
PathhyCunha <149610564+PathhyCunha@users.noreply.github.com>
2023-12-03 23:25:36 -0300

    Aula 7

---
+++ meuumjava.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("action.bnt");
+
+button.addEventListener("click",function(){
+    var menu = document.getElementById("menu");
+    if (menu.style.display==="none");
+    {
+        menu.style.display="block";
+    } 
+});
